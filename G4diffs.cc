560b6b4c040a1437e770d3f73aa417b0db0b513d
AJ Biffl <ajbiffl219@gmail.com>
2023-11-17 22:36:26 -0700

    printing final energy

--- G4diffs.cc
+++ G4diffs.cc
@@ -11,7 +11,12 @@
 #include "MinimalUserInitialization.hh"
 
 #include "G4HadronElasticProcess.hh"
+#include "G4DynamicParticle.hh"
+#include "G4HadProjectile.hh"
+#include "G4HadFinalState.hh"
+#include "G4Track.hh"
 
+#include "G4ParticleHPManager.hh"
 
 
 int main(int argc, char **argv) {
@@ -29,7 +34,7 @@ int main(int argc, char **argv) {
         mat = argv[1];
         G4cout << "set material to " << mat << G4endl;
     } else {
-        mat = "G4_H";
+        mat = "G4_Si";
     }
 
     //runManager->SetVerboseLevel(0);
@@ -48,28 +53,66 @@ int main(int argc, char **argv) {
 
     const G4ParticleDefinition *theNeutron = G4Neutron::Definition();
 
+    // initialize physics list
     runManager->Initialize();
+    G4cout << "initialized" << G4endl;
 
     G4ProcessManager *theMan = theNeutron->GetProcessManager();
     G4ProcessVector *procList = theMan->GetProcessList();
     G4HadronElasticProcess *elasticProc = static_cast<G4HadronElasticProcess*>((*procList)[1]);
+    //std::vector<G4HadronicInteraction*> interactionList = elasticProc->GetHadronicInteractionList();
+    G4HadronicInteraction* elasticInteraction = elasticProc->GetHadronicInteractionList()[0];
 
-    for (size_t p = 1; p < 5; ++p) {
-        std::vector<G4HadronicInteraction*> interactionList = static_cast<G4HadronicProcess*>((*procList)[p])->GetHadronicInteractionList();
+    G4ThreeVector origin = G4ThreeVector(0.,0.,0.);
+    
+    G4StepPoint *thePoint = new G4StepPoint();
+    thePoint->SetPosition(origin);
+    thePoint->SetMaterial(material);
 
-        G4cout << (*procList)[p]->GetProcessName() << " : " << interactionList.size() << " interactions" << G4endl;
+    G4Step *theStep = new G4Step();
+    theStep->SetPreStepPoint(thePoint);
+    
+    G4double initialEnergy = 1.*MeV;
 
-        for (size_t i = 0; i < interactionList.size(); ++i) {
-            
-            G4cout << "Model " << interactionList[i]->GetModelName() << G4endl;
+    G4DynamicParticle *dynamicNeutron = new G4DynamicParticle(theNeutron, G4ThreeVector(0.,0.,1.), 0.);
+    dynamicNeutron->SetKineticEnergy(initialEnergy);
+    G4Track *neutronTrack = new G4Track(dynamicNeutron, 0., origin);
+    neutronTrack->SetStep(theStep);
 
-            interactionList[i]->ModelDescription(G4cout);
-        }
-    }
+    G4HadProjectile *projectile = new G4HadProjectile(*neutronTrack);
+    G4cout << "created projectile" << G4endl;
+    
+    // get G4Nucleus
+    G4Nucleus *materialNucleus = new G4Nucleus(material);
+    G4CrossSectionDataStore *elasticDataStore = elasticProc->GetCrossSectionDataStore();
+    const G4Element *theElement = elasticDataStore->SampleZandA(dynamicNeutron, material, *materialNucleus);
+    elasticDataStore->SampleZandA(dynamicNeutron, material, *materialNucleus);
+    G4cout << "selected isotope" << G4endl;
 
-    G4cout << "done" << G4endl;
+    
+
+    //for (size_t i = 0; i < interactionList.size(); ++i) {
+    //    G4cout << "Model " << interactionList[i]->GetModelName() << G4endl;
+    //    interactionList[i]->ModelDescription(G4cout);
+    //}
+
+    G4cout << elasticInteraction->GetModelName() << G4endl;
+    elasticInteraction->SetVerboseLevel(10);
+
+    auto thing = projectile->GetMaterial();
+    if (!thing) { G4cout << "there's your problem" << G4endl;}
+
+    G4HadFinalState *neutronFS = elasticInteraction->ApplyYourself(*projectile, *materialNucleus);
+
+    G4cout << neutronFS->GetEnergyChange() << G4endl; // from usage in G4HadronicProcess::FillResult(), GetEnergyChange() returns the final energy
+    
+
+
+    
 
     delete runManager;
+
+    G4cout << "done" << G4endl;
 
     return 0;
 }
