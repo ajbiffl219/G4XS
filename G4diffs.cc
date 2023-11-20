1dc1210e66afbac7128688e5c1525c340775b0a7
AJ Biffl <ajbiffl219@gmail.com>
2023-11-20 11:02:07 -0700

    testing mode for Si-28, specify neutron energy

--- G4diffs.cc
+++ G4diffs.cc
@@ -21,6 +21,16 @@
 #include "G4ParticleHPManager.hh"
 
 
+void writeArray(G4String fileName, G4double *data, G4int N) {
+    std::ofstream os(fileName);
+    os << std::setprecision(17);
+    for (int i = 0; i < N; ++i) {
+        os << i << " " << data[i] << G4endl;
+    }
+    os.close();
+}
+
+
 int main(int argc, char **argv) {
 
     CLHEP::HepRandom::setTheSeed(time(NULL));
@@ -33,13 +43,16 @@ int main(int argc, char **argv) {
     G4cout << "sequential mode" << G4endl;
 #endif
 
-    G4String mat;
+    G4String mat = "G4_Si";
+    G4double initialEnergy = 2.*MeV;
     if (argc == 2) {
-        mat = argv[1];
-        G4cout << "set material to " << mat << G4endl;
+        //mat = argv[1];
+        initialEnergy = std::stof(argv[1])*MeV;
     } else {
         mat = "G4_Si";
     }
+
+    G4cout << "set material to " << mat << G4endl;
 
     //runManager->SetVerboseLevel(0);
     //G4VModularPhysicsList *physList = new NeutronPhysicsList;
@@ -76,9 +89,11 @@ int main(int argc, char **argv) {
     G4Step *theStep = new G4Step();
     theStep->SetPreStepPoint(thePoint);
     
-    G4double initialEnergy = 1.*MeV;
+    //G4double initialEnergy = 2.*MeV;
 
     G4DynamicParticle *dynamicNeutron = new G4DynamicParticle(theNeutron, G4ThreeVector(0.,0.,1.), 0.);
+    
+    
     dynamicNeutron->SetKineticEnergy(initialEnergy);
     G4Track *neutronTrack = new G4Track(dynamicNeutron, 0., origin);
     neutronTrack->SetStep(theStep);
@@ -90,8 +105,10 @@ int main(int argc, char **argv) {
     G4Nucleus *materialNucleus = new G4Nucleus;
     G4CrossSectionDataStore *elasticDataStore = elasticProc->GetCrossSectionDataStore();
     elasticDataStore->ComputeCrossSection(dynamicNeutron, material); // need to compute cross sections in material before sampling Z/A
-    const G4Element *theElement = elasticDataStore->SampleZandA(dynamicNeutron, material, *materialNucleus);
-    elasticDataStore->SampleZandA(dynamicNeutron, material, *materialNucleus);
+    //const G4Element *theElement = elasticDataStore->SampleZandA(dynamicNeutron, material, *materialNucleus);
+    do {
+        elasticDataStore->SampleZandA(dynamicNeutron, material, *materialNucleus);
+    } while (materialNucleus->GetA_asInt() != 28);
     G4cout << "selected isotope" << G4endl;
 
     
@@ -104,15 +121,24 @@ int main(int argc, char **argv) {
     G4cout << elasticInteraction->GetModelName() << G4endl;
     elasticInteraction->SetVerboseLevel(10);
 
-    auto thing = projectile->GetMaterial();
-    if (!thing) { G4cout << "there's your problem" << G4endl;}
+    //auto thing = projectile->GetMaterial();
+    //if (!thing) { G4cout << "there's your problem" << G4endl;}
 
-    G4HadFinalState *neutronFS = elasticInteraction->ApplyYourself(*projectile, *materialNucleus);
+    const G4int N = 100000;
+    G4double finalEnergies[N];
+    G4HadFinalState *neutronFS;
 
-    G4cout << neutronFS->GetEnergyChange() << G4endl; // from usage in G4HadronicProcess::FillResult(), GetEnergyChange() returns the final energy
+    for (int i = 0; i <N; ++i) {
+        neutronFS = elasticInteraction->ApplyYourself(*projectile, *materialNucleus);
+        finalEnergies[i] = neutronFS->GetEnergyChange();
+    }
+
+    //G4HadFinalState *neutronFS = elasticInteraction->ApplyYourself(*projectile, *materialNucleus);
+
+    //G4cout << neutronFS->GetEnergyChange() << G4endl; // from usage in G4HadronicProcess::FillResult(), GetEnergyChange() returns the final energy
     
-
-
+    G4String fileName = "finalEnergies_Si28_" + std::to_string(initialEnergy) + ".txt";
+    writeArray(fileName, finalEnergies, N);
     
 
     delete runManager;
